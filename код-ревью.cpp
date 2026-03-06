9d23d0c547b1e8413c33f33c0597ebae3d9d981e
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-06 16:05:19 +0500

    Add files via upload

---
+++ код-ревью.cpp
@@ -0,0 +1,46 @@
+﻿#include "head.h"
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    // Флаг для продолжения
+    bool bContinue = false;
+
+    std::cout << "Выполняем запуск Task1" << std::endl;
+    Task1();
+
+    std::cout << "Продолжить выполнение следующего задания? (1 - да, 0 - нет): ";
+    std::cin >> bContinue;
+
+    if (bContinue)
+    {
+        std::cout << "Выполняем запуск Task2" << std::endl;
+        Task2();
+
+        std::cout << "Продолжить выполнение следующего задания? (1 - да, 0 - нет): ";
+        std::cin >> bContinue;
+
+        if (bContinue)
+        {
+            std::cout << "Выполняем запуск Task3" << std::endl;
+            Task3();
+
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            std::cout << "Продолжить выполнение следующего задания? (1 - да, 0 - нет): ";
+            std::cin >> bContinue;
+
+            if (bContinue)
+            {
+                std::cout << "Выполняем запуск Task4" << std::endl;
+
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                Task4();
+            }
+        }
+    }
+
+    return 0;
+}
